d7772ec94e7ff1aacb271b6a97558fb4d1468fe1
maugan-robert <maugan.robert@edu.univ-fcomte.fr>
2024-04-12 16:08:03 +0200

    codes accordeons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,20 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nemo, aut numquam unde voluptatibus, dolores sit corporis, debitis officiis et nisi. Quos nemo quae repellendus unde, neque sed libero mollitia?</p>
+  </section>
+
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Doloribus dolore dolor vitae aspernatur ut obcaecati explicabo cumque molestiae laboriosam mollitia vero perspiciatis, dignissimos, eveniet illum recusandae eius doloremque libero suscipit ipsa. Consequuntur eos reprehenderit quis ipsa modi et.</p>
+  </section>
+  
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Deleniti, atque perspiciatis quasi dolores itaque reprehenderit reiciendis obcaecati consequuntur! Saepe, reprehenderit quia? Eaque unde hic maiores reprehenderit. Aut unde sit ea enim nam harum cupiditate quisquam obcaecati architecto doloremque!</p>
+  </section>
+
 </template>
