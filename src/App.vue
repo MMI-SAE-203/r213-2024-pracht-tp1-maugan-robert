631866cfc741df45cc83a1f244e93ba47096f232
maugan-robert <maugan.robert@edu.univ-fcomte.fr>
2024-04-12 16:03:56 +0200

    v-show et menu avec pointerdown

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,6 @@ const menuIsOpen = ref(false)
   <header>
     <nav>
       <ul>
-  <button
-    aria-controls="mainNav"
-    aria-expanded="true"
-    class="rounded-full border-2 border-red-600 bg-red-300 px-2">menu</button>
-  <nav id="mainNav">
-    <ul>
-      <li><a href="#">item 1</a></li>
-      <li><a href="#">item 2</a></li>
-      <li><a href="#">item 3</a></li>
-    </ul>
-  </nav>
         <li>
           <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
         </li>
@@ -35,4 +24,16 @@ const menuIsOpen = ref(false)
       <component :is="Component" />
     </Suspense>
   </RouterView>
+    <button
+    @pointerdown="menuIsOpen = !menuIsOpen"
+    aria-controls="mainNav"
+    aria-expanded="true"
+    class="rounded-full border-2 border-red-600 bg-red-300 px-2">menu</button>
+  <nav id="mainNav" v-show="menuIsOpen">
+    <ul>
+      <li><a href="#">item 1</a></li>
+      <li><a href="#">item 2</a></li>
+      <li><a href="#">item 3</a></li>
+    </ul>
+  </nav>
 </template>
